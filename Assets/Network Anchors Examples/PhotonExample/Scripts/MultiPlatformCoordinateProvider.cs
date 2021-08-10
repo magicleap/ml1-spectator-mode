07b62c703c41008498bb8846fcca06f3e9913680
Krystian Babilinski <kbabilinski@magicleap.com>
2021-08-10 12:29:14 -0400

    Fix standalone functionality

--- Assets/Network Anchors Examples/PhotonExample/Scripts/MultiPlatformCoordinateProvider.cs
+++ Assets/Network Anchors Examples/PhotonExample/Scripts/MultiPlatformCoordinateProvider.cs
@@ -8,13 +8,20 @@ public class MultiPlatformCoordinateProvider : MonoBehaviour, IGenericCoordinate
     [Header("Providers")]
     public MLGenericCoordinateProvider MagicLeapProvider;
     public StandaloneCoordinateProvider StandaloneProvider;
-    [Header("Debugging")] [SerializeField]
+ 
     private bool _forceStandalone;
 
+    void Awake()
+    {
+#if UNITY_EDITOR
+        _forceStandalone = true;
+#endif
+    }
     public Task<List<GenericCoordinateReference>> RequestCoordinateReferences(bool refresh)
     {
+
 #if PLATFORM_LUMIN
-        if(_forceStandalone)
+        if (_forceStandalone)
             return StandaloneProvider.RequestCoordinateReferences(refresh);
 
         return MagicLeapProvider.RequestCoordinateReferences(refresh);
